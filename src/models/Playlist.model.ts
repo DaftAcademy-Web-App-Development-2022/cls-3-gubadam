8e3c67a88f83e8933a86a36e9537bab40eb56d28
Adam <a0d1mq@gmail.com>
2022-11-30 12:23:41 +0100

    Added endpoints for playlists operations

--- src/models/Playlist.model.ts
+++ src/models/Playlist.model.ts
@@ -1,10 +1,25 @@
+import { model, models, Schema } from "mongoose";
+import { DEFAULT_CARD_COLOR } from "~/config/common.config";
+
 export interface PlaylistModel {
-  color?: string;
-  name: string;
-  owner: string;
-  slug: string;
-  spotifyId: string;
-  upvote: number;
+    color?: string;
+    name: string;
+    owner: string;
+    slug: string;
+    spotifyId: string;
+    upvote: number;
 }
 
 export type PlaylistModelWithId = PlaylistModel & { id: string };
+
+const schema = new Schema<PlaylistModel>({
+    color: { type: String, default: DEFAULT_CARD_COLOR },
+    name: { type: String, required: true },
+    owner: { type: String, required: true },
+    slug: { type: String, required: true },
+    spotifyId: { type: String, required: true },
+    upvote: { type: Number, required: true },
+});
+
+export const Playlist =
+    models.PlaylistModel || model<PlaylistModel>("PlaylistModel", schema);
--- src/pages/api/playlist/index.ts
+++ src/pages/api/playlist/index.ts
@@ -1,12 +1,45 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { dbConnect } from "~/libraries/mongoose.library";
+import { Playlist } from "~/models/Playlist.model";
+import { DEFAULT_CARD_COLOR } from "~/config/common.config";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
+    req: NextApiRequest,
+    res: NextApiResponse<any>
 ) {
-  await dbConnect();
-  res.status(200).send({});
+    await dbConnect();
+
+    if (req.method === "GET") {
+        const playlists = await getPlaylists();
+        res.status(200).send({ data: playlists });
+    } else if (req.method === "POST") {
+        await createPlaylist(req.body);
+        const playlists = await getPlaylists();
+        const n = playlists.length;
+        res.status(201).send({
+            data: playlists[n - 1],
+        });
+    }
+}
+
+async function getPlaylists() {
+    const result = await Playlist.find();
+    return result.map((doc) => {
+        const playlist = doc.toObject();
+        return {
+            color: playlist.color || DEFAULT_CARD_COLOR,
+            id: playlist._id.toString(),
+            name: playlist.name,
+            owner: playlist.owner,
+            slug: playlist.slug,
+            spotifyId: playlist.spotifyId,
+            upvote: playlist.upvote,
+        };
+    });
+}
+
+async function createPlaylist(obj: unknown) {
+    await Playlist.create(obj);
 }
 
 export type Response = any;
